79f6553f83d0b84720f28055fe03c018eb1747e8
Emmanuel, Oluwatobi <oluwatobiemmanuel@my.unt.edu>
2025-06-27 21:50:08 -0500

    feat: implement CreditCard class with basic validate method

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pandas as pd
 
 df = pd.read_csv("hotels.csv", dtype={"id": str})
 
+df_cards = pd.read_csv("cards.csv", dtype=str).to_dict(orient="records")
+
 
 class Hotel:
     def __init__(self, hotel_id):
@@ -27,7 +29,6 @@ class Ticket:
         self.customer_name = customer_name
         self.hotel = hotel_object
 
-
     def generate(self):
         content = f"""
         Thank you for your reservation!
@@ -39,15 +40,33 @@ class Ticket:
         return content
 
 
+class CreditCard:
+    def __init__(self, number):
+        self.number = number
+
+    def validate(self, expiration, holder, cvc):
+        card_data = {"number": self.number, "expiration": expiration, "holder": holder, "cvc": cvc}
+
+        if card_data in df_cards:
+            return True
+        else:
+            return False
+
+
 print(df)
 hotel_ID = input("Enter the id of the hotel: ")
 hotel = Hotel(hotel_ID)
 
 if hotel.available():
-    hotel.book()
+    credit_card = CreditCard(number="1234")
 
-    name = input("Enter your name: ")
-    ticket = Ticket(customer_name=name, hotel_object=hotel)
-    print(ticket.generate())
+    if credit_card.validate(expiration="12/26", holder="JOHN DOE", cvc="123"):
+        hotel.book()
+
+        name = input("Enter your name: ")
+        ticket = Ticket(customer_name=name, hotel_object=hotel)
+        print(ticket.generate())
+    else:
+        print("There was an issue validating your credit card.")
 else:
     print("Hotel is currently closed.")
